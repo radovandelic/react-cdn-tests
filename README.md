3eb1b79caa28153ea24f3bd235f255af0665bc3c
Radovan Delic <infamouszgbgd@yahoo.com>
2017-11-05 16:29:28 +0100

    bundle everything

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This is an experiment to test the performance of different react setups.
 
+1. Bundle everything:
+    * 
+    * 
+    * 
--- src/index.js
+++ src/index.js
@@ -4,20 +4,10 @@ import './index.css';
 import App from './App';
 import { AceEditor } from './App';
 import registerServiceWorker from './registerServiceWorker';
-//var React = require("./react").React;
-//var ReactDOM = require("./react").ReactDOM;
 
 
 
 render(<App />, document.getElementById('root'));
 render(<AceEditor />, document.getElementById('editor'));
 
-/*window.onload = function () {
-    var ace = window.ace;
-    var editor = ace.edit("editor");
-    //editor.setValue("testing");
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
-
-}*/
 registerServiceWorker();
